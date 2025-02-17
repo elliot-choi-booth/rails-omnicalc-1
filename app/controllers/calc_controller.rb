6745030198d7e0633d0491f2d5a52e8a6e7e5f68
elliot-choi-booth <elliotc0@chicagobooth.edu>
2025-02-17 19:07:05 +0000

    added in payments

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -12,4 +12,33 @@ class CalcController < ApplicationController
     render({ :template => "calc_templates/square_results"})
   end
 
+  def square_root
+    render({ :template => "calc_templates/square_root"})
+  end
+
+  def square_root_results
+    @sqrt_input = params.fetch("users_number").to_f
+
+    @sqrt_results = @sqrt_input ** 0.5
+
+    render({ :template => "calc_templates/square_root_results"})
+  end
+
+  def payment
+    render({ :template => "calc_templates/payment"})
+  end
+
+  def payment_results
+    @apr = params.fetch("apr").to_f / 1200
+
+    @years = params.fetch("years").to_i
+    @years_converted = @years * 12
+
+    @principal = params.fetch("principal").to_f
+
+    @result = @apr * @principal / (1 - (1 + @apr)) ** (@years_converted - 1)
+
+    render({ :template => "calc_templates/payment_results"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
 
   get("/square/results", :controller => "calc", :action => "square_results")
 
+  get("/square_root/new", :controller => "calc", :action => "square_root")
+
+  get("/square_root/results", :controller => "calc", :action => "square_root_results")
+
+  get("/payment/new", :controller => "calc", :action => "payment")
+
+  get("/payment/results", :controller => "calc", :action => "payment_results")
+
 end
